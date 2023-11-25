2a19da2848439839988dac6a593be0fc89276038
Anzo52 <azollner2014@gmail.com>
2023-11-24 21:13:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, this is the home page of Maigret Web Interface.'
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        # Extract data from the form
+        username = request.form['username']
+        # Perform search with Maigret here
+        results = perform_maigret_search(username)
+        return render_template('results.html', results=results)
+    else:
+        # For a GET request, just display the search form
+        return render_template('search_form.html')
+
+def perform_maigret_search(username):
+    # Your Maigret search logic goes here
+    return search_results
 
 if __name__ == '__main__':
     app.run(debug=True)
