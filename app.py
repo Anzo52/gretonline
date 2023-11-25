2f6c7b1ff5309853f0946b5d2f80f8f2d8c35563
Anzo52 <azollner2014@gmail.com>
2023-11-24 21:28:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,33 @@ def search():
         # For a GET request, just display the search form
         return render_template('search_form.html')
 
+
+
+
 def perform_maigret_search(username):
-    # Your Maigret search logic goes here
-    return search_results
+    # Initialize Maigret database
+    db = MaigretDatabase().load_from_file()
+
+    # List of sites to search on (optional, remove to search on all sites)
+    sites = [MaigretSite('Twitter'), MaigretSite('Facebook')]
+
+    # Perform the search
+    try:
+        result = search(username, db, sites_list=sites)
+        # Format and return the results
+        return format_maigret_results(result)
+    except Exception as e:
+        print(f"Error during search: {e}")
+        return None
+
+def format_maigret_results(results):
+    # Format the results into a more readable form
+    # This is a placeholder, you can format the results as you need
+    formatted_results = {}
+    for site_name, data in results.items():
+        if data['exists']:
+            formatted_results[site_name] = data['url_user']
+    return formatted_results
 
 if __name__ == '__main__':
     app.run(debug=True)
