912c5f7d00fd3920b94a2a99718e7069901f532d
Anzo52 <andy@zollnersolutions.com>
2023-11-25 15:49:22 -0700

    m

--- app.py
+++ app.py
@@ -4,38 +4,42 @@ from gretlogger import setup_logger
 
 app = Flask(__name__)
 
-@app.route('/search', methods=['GET', 'POST'])
-def search_route():
-    if request.method != 'POST':
-        # For a GET request, just display the search form
-        return render_template('search_form.html')
-    # Extract data from the form
-    username = request.form['username']
-    # Perform search with Maigret here
-    results = perform_maigret_search(username)
-    return render_template('results.html', results=results)
 
-def perform_maigret_search(username):
+@app.route("/search", methods=["GET", "POST"])
+async def search_route():
+    if request.method != "POST":
+        # For a GET request, just display the search form
+        return render_template("search_form.html")
+    # Extract data from the form
+    username = request.form["username"]
+    # Perform search with Maigret here
+    results = await perform_maigret_search(username)
+    return render_template("results.html", results=results)
+
+
+async def perform_maigret_search(username):
     logger = setup_logger(__name__)
     # Initialize Maigret database
-    maigret_sites_file = './env/lib/python3.9/site-packages/maigret/resources/data.json'
+    maigret_sites_file = "./env/lib/python3.9/site-packages/maigret/resources/data.json"
     db = MaigretDatabase().load_from_file(maigret_sites_file)
 
     # Perform the search
     try:
-        result = maigret_search(username, db, logger)
+        result = await maigret_search(username, db, logger)
         # Format and return the results
         return format_maigret_results(result)
     except Exception as e:
         logger.error(f"Error while searching for {username}: {e}")
         return None
 
+
 def format_maigret_results(results):
     return {
-        site_name: data['url_user']
+        site_name: data["url_user"]
         for site_name, data in results.items()
-        if data['exists']
+        if data["exists"]
     }
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
